400293fef2e140a878ca1b9c119e70c06e28c4bc
João Pedro <124808689+joaotif@users.noreply.github.com>
2025-01-09 11:34:06 -0300

    Add files via upload

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Desenvolvido por João P. Soares © 2023
+        </footer>
+    )
+}
+
+export default Footer
